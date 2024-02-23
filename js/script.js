e1d64ce602b96cbee589ed6b260068876afff4f3
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-02-23 17:57:11 +0100

    added navigation disappearing after clicking on item

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const navigation = document.querySelector(".js-navigation");
 const burgerButton = document.querySelector(".js-burger-button");
+const allNavigationItems = document.querySelectorAll(".js-navigation__item");
 
 const toggleNavigation = () => {
 	navigation.classList.toggle("navigation--active");
+
+	allNavigationItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			navigation.classList.remove("navigation--active");
+		});
+	});
 };
 
 burgerButton.addEventListener("click", toggleNavigation);
