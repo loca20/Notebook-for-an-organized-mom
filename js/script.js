b19404767eb222883f932e27ba30ea0205443c6a
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-02-23 16:53:21 +0100

    toggleNavigation added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const navigation = document.querySelector(".js-navigation");
+const burgerButton = document.querySelector(".js-burger-button");
 
+const toggleNavigation = () => {
+	navigation.classList.toggle("navigation--active");
+};
+
+burgerButton.addEventListener("click", toggleNavigation);
