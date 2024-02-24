0554f1148e507070a6607e291475c1db6bbd798f
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-02-24 18:31:20 +0100

    added showSection function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const navigation = document.querySelector(".js-navigation");
 const burgerButton = document.querySelector(".js-burger-button");
 const allNavigationItems = document.querySelectorAll(".js-navigation__item");
+const sections = document.querySelectorAll(".section");
+
 
 const toggleNavigation = () => {
 	navigation.classList.toggle("navigation--active");
@@ -12,4 +14,18 @@ const toggleNavigation = () => {
 	});
 };
 
+const showSection = () => {
+    allNavigationItems.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            sections.forEach((section, sectionIndex) => {
+                section.classList.remove("section--active");
+                if (index === sectionIndex) {
+                    section.classList.add("section--active");
+                }
+            });
+        });
+    });
+};
+
 burgerButton.addEventListener("click", toggleNavigation);
+showSection();
