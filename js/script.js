b7a4ea5388815c7a7d11a2737d81ef2a664c574e
Anna Lukaszczyk <normalnie4@tlen.pl>
2024-02-24 19:35:20 +0100

    added action on clicking the arrow

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ const navigation = document.querySelector(".js-navigation");
 const burgerButton = document.querySelector(".js-burger-button");
 const allNavigationItems = document.querySelectorAll(".js-navigation__item");
 const sections = document.querySelectorAll(".section");
-
+const arrowDown = document.querySelector(".js-arrow-down");
+const sectionAboutBaby = document.querySelector(".js-section-about-baby");
 
 const toggleNavigation = () => {
 	navigation.classList.toggle("navigation--active");
@@ -15,17 +16,22 @@ const toggleNavigation = () => {
 };
 
 const showSection = () => {
-    allNavigationItems.forEach((item, index) => {
-        item.addEventListener("click", () => {
-            sections.forEach((section, sectionIndex) => {
-                section.classList.remove("section--active");
-                if (index === sectionIndex) {
-                    section.classList.add("section--active");
-                }
-            });
-        });
-    });
+	allNavigationItems.forEach((item, index) => {
+		item.addEventListener("click", () => {
+			sections.forEach((section, sectionIndex) => {
+				section.classList.remove("section--active");
+				if (index === sectionIndex) {
+					section.classList.add("section--active");
+				}
+			});
+		});
+	});
+};
+
+const arrowDownAction = () => {
+	sectionAboutBaby.classList.add("section--active");
 };
 
 burgerButton.addEventListener("click", toggleNavigation);
 showSection();
+arrowDown.addEventListener("click", arrowDownAction);
